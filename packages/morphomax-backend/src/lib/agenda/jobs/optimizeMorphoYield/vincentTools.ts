ba123fe5252cea261c965ba1862a0ff8a70c5915
FedericoAmura <federicoamura@gmail.com>
2025-07-18 19:50:11 +0200

    feat: export disconnectVincentToolClients from vincentTools

--- packages/morphomax-backend/src/lib/agenda/jobs/optimizeMorphoYield/vincentTools.ts
+++ packages/morphomax-backend/src/lib/agenda/jobs/optimizeMorphoYield/vincentTools.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 
 import { LIT_RPC } from '@lit-protocol/constants';
-import { getVincentToolClient } from '@lit-protocol/vincent-app-sdk';
+import { getVincentToolClient, disconnectVincentToolClients } from '@lit-protocol/vincent-app-sdk';
 import { bundledVincentTool as erc20ApprovalTool } from '@lit-protocol/vincent-tool-erc20-approval';
 import { bundledVincentTool as morphoTool } from '@lit-protocol/vincent-tool-morpho';
 import { bundledVincentTool as uniswapSwapTool } from '@lit-protocol/vincent-tool-uniswap-swap';
@@ -9,6 +9,8 @@ import { bundledVincentTool as uniswapSwapTool } from '@lit-protocol/vincent-too
 import { env } from '../../../env';
 
 const { VINCENT_DELEGATEE_PRIVATE_KEY } = env;
+
+export { disconnectVincentToolClients };
 
 export const ethersSigner = new ethers.Wallet(
   VINCENT_DELEGATEE_PRIVATE_KEY,
